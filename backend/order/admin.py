aaaa90fae12ffdfd14155d54d5f9b531a9a2d3ed
trsv-dev <tarasov.itc@yandex.ru>
2024-04-16 21:14:06 +0300

    A button has been added to the admin panel to enable/disable image preview in an order.

--- backend/order/admin.py
+++ backend/order/admin.py
@@ -1,14 +1,36 @@
 from django.conf import settings
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.utils.html import format_html
 
 from order.models import Order, OrderItem
 
 
 class ItemsInLine(admin.TabularInline):
+    """Класс для отображения товаров в заказе внутри 'OrderAdmin'."""
+
     model = OrderItem
     extra = 0
     ordering = ('id',)
-    readonly_fields = ('price_per_item', 'price_for_all_items',)
+    readonly_fields = ('show_image_preview', 'price_per_item',
+                       'price_for_all_items',)
+
+    def __init__(self, model, admin_site):
+        """
+        Конструктор класса. Используем для добавления атрибута 'self.request'.
+        """
+
+        self.request = None
+        super().__init__(model, admin_site)
+
+    def get_queryset(self, request):
+        """
+        Переопределяем метод для добавления в атрибут
+        класса 'self.request' админского 'request'.
+        """
+
+        self.request = request
+        return super().get_queryset(request)
 
     def price_for_all_items(self, obj):
         """
@@ -26,6 +48,24 @@ class ItemsInLine(admin.TabularInline):
         return f'{obj.item.discount_price if obj.item.is_discount else obj.item.price:,} руб.'
 
     price_per_item.short_description = 'Стоимость за единицу'
+
+    def show_image_preview(self, obj):
+        """Показать превью изображения товара в заказе."""
+
+        # Получаем контекст администратора благодаря переопределенному
+        # 'get_queryset'
+        request = self.request
+        show_preview = request.session.get('show_preview', False)
+        if show_preview:
+            image = obj.item.image
+            if image:
+                return format_html(
+                    '<img src="{}" '
+                    'style="max-width: 180px; max-height: 180px; '
+                    'object-fit: cover;" />', image.url
+                )
+        return 'Превью отключены'
+    show_image_preview.short_description = 'Превью'
 
 
 @admin.register(Order)
@@ -88,3 +128,28 @@ class OrderAdmin(admin.ModelAdmin):
         return sum(item.quantity for item in obj.orderitem_set.all())
 
     show_items_total_quantity.short_description = 'Общее количество'
+
+    def response_change(self, request, obj):
+        """
+        Переопределяем порядок действий при редактировании заказа, а
+        конкретнее - при нажатии на кнопку "Вкл./выкл. превью" включается или
+        отключается отображение изображений в заказе.
+        Заказ остается на странице редактирования.
+        """
+        if '_show_preview' in request.POST:
+            show_preview = request.session.get('show_preview', False)
+            request.session['show_preview'] = not show_preview
+            if show_preview:
+                self.message_user(request, 'Отображение изображений '
+                                           'товара в заказе выключено')
+            else:
+                self.message_user(request, 'Отображение изображений '
+                                           'товара в заказе включено')
+
+            # Остаемся на текущей странице если редактируем.
+            # Если сохраняем, то поведение без изменений.
+            if '_save' in request.POST:
+                pass
+
+            return HttpResponseRedirect(request.path_info)
+        return super().response_change(request, obj)
